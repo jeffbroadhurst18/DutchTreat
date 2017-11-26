5e591dc797ac0c750bb09220d737739d69b67d13
jeffbroadhurst18 <jeffbroadhurst18@outlook.com>
2017-11-26 19:57:11 +0000

    Add project files.

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DutchTreat.ViewModels;
+
+namespace DutchTreat.Controllers
+{
+	public class AppController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet("contact")] //can access via /contact not /app/contact
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[HttpPost("contact")]
+		public IActionResult Contact(ContactViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				_mailService.SendMail("jeffbroadhurst18@outlook.com", model.Subject, $"From: {model.Name} {model.Email}, Message: {model.Message}");
+			}
+			
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewBag.Title = "About Us";
+			return View();
+		}
+	}
+}
---
+++ DutchTreat/Services/NullMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Services
+{
+	public class NullMailService
+	{
+		public void SendMessage(string to, string subject, string body)
+		{
+		}
+	}
+}
